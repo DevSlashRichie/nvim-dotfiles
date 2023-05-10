b994ae31b4701bd04f347043855224afa1c13473
DevSlashRichie <me@slashrichie.dev>
2023-05-09 23:24:39 -0600

    f

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,3 +1,7 @@
+require('rose-pine').setup({
+	dim_nc_background = true,
+	disable_background = true,
+})
 function ColorMyPencils(color)
 	color = color or "rose-pine"
 	vim.cmd.colorscheme(color)
--- lua/ricardo/packer.lua
+++ lua/ricardo/packer.lua
@@ -5,93 +5,101 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
 
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use({
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  })
+    use({
+	'nvim-telescope/telescope.nvim', tag = '0.1.1',
+	requires = { {'nvim-lua/plenary.nvim'} }
+    })
 
-  use({ 
-      'rose-pine/neovim',
-      as = 'rose-pine',
-      config = function()
-          vim.cmd('colorscheme rose-pine')
-      end
-  })
+    use({ 
+	'rose-pine/neovim',
+	as = 'rose-pine',
+	config = function()
+	    vim.cmd('colorscheme rose-pine')
+	end
+    })
 
-  use ({ 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'} })
+    use ({ 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'} })
 
-  use('nvim-treesitter/nvim-treesitter-context')
+    use('nvim-treesitter/nvim-treesitter-context')
 
-  use('theprimeagen/harpoon')
+    use('theprimeagen/harpoon')
 
-  use('mbbill/undotree')
+    use('mbbill/undotree')
 
-  use('tpope/vim-fugitive')
+    use('tpope/vim-fugitive')
 
-  use('RRethy/vim-illuminate')
+    use('RRethy/vim-illuminate')
 
-  use('easymotion/vim-easymotion')
+    use('easymotion/vim-easymotion')
 
-  use('f-person/git-blame.nvim')
+    use('f-person/git-blame.nvim')
 
-  --[[
+    --[[
   use({
       'stevearc/oil.nvim', config = function()
-          require('oil').setup() 
+	  require('oil').setup() 
       end
   })
   --]]
 
-  use({
-      'jose-elias-alvarez/null-ls.nvim',
-      requires = {
-          {'nvim-lua/plenary.nvim'}
-      }
-  })
+    use({
+	'jose-elias-alvarez/null-ls.nvim',
+	requires = {
+	    {'nvim-lua/plenary.nvim'}
+	}
+    })
 
-  use('neovim/nvim-lspconfig')
+    use('neovim/nvim-lspconfig')
 
-  use('simrat39/rust-tools.nvim')
+    use('simrat39/rust-tools.nvim')
 
-  use({
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v1.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},             -- Required
-		  {'williamboman/mason.nvim'},           -- Optional
-		  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    use({
+	'VonHeikemen/lsp-zero.nvim',
+	branch = 'v1.x',
+	requires = {
+	    -- LSP Support
+	    {'neovim/nvim-lspconfig'},             -- Required
+	    {'williamboman/mason.nvim'},           -- Optional
+	    {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},         -- Required
-		  {'hrsh7th/cmp-nvim-lsp'},     -- Required
-		  {'hrsh7th/cmp-buffer'},       -- Optional
-		  {'hrsh7th/cmp-path'},         -- Optional
-		  {'saadparwaiz1/cmp_luasnip'}, -- Optional
-		  {'hrsh7th/cmp-nvim-lua'},     -- Optional
+	    -- Autocompletion
+	    {'hrsh7th/nvim-cmp'},         -- Required
+	    {'hrsh7th/cmp-nvim-lsp'},     -- Required
+	    {'hrsh7th/cmp-buffer'},       -- Optional
+	    {'hrsh7th/cmp-path'},         -- Optional
+	    {'saadparwaiz1/cmp_luasnip'}, -- Optional
+	    {'hrsh7th/cmp-nvim-lua'},     -- Optional
 
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},             -- Required
-		  {'rafamadriz/friendly-snippets'}, -- Optional
-	  }
-  })
+	    -- Snippets
+	    {'L3MON4D3/LuaSnip'},             -- Required
+	    {'rafamadriz/friendly-snippets'}, -- Optional
+	}
+    })
 
-  use('github/copilot.vim')
-  use('tpope/vim-sleuth')
+    use('github/copilot.vim')
+
+    --- Used for fixing indents
+    use('tpope/vim-sleuth')
 
 
-  use({
-      'folke/trouble.nvim',
-      config = function()
-          require('trouble').setup {
-              icons = false,
-          }
-      end
-  })
-   
+    use({
+	'folke/trouble.nvim',
+	config = function()
+	    require('trouble').setup {
+		icons = false,
+	    }
+	end
+    })
 
+    use({
+	"andythigpen/nvim-coverage",
+	requires = "nvim-lua/plenary.nvim",
+	config = function()
+	    require("coverage").setup()
+	end,
+    })
 
 end)
