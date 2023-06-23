a72dbd91c1a3ffac1b58460e3f4e999035cff698
DevSlashRichie <me@slashrichie.dev>
2023-06-23 15:08:17 -0600

    feat: changed defs and refs to trouble

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -5,3 +5,12 @@ vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
 vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle document_diagnostics<cr>",
   {silent = true, noremap = true}
 )
+
+vim.keymap.set("n", "<leader>vrr", "<cmd>TroubleToggle lsp_references<cr>",
+  {silent = true, noremap = true}
+)
+
+
+vim.keymap.set("n", "<C-b>", "<cmd>TroubleToggle lsp_definitions<cr>",
+  {silent = true, noremap = true}
+)
